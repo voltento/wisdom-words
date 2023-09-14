709ee8ad3c12d1815ad5238cab2ca5f1cc4c0b98
vladimir.dumanovskii <vladimir.dumanovskii@careem.com>
2023-09-14 11:38:49 +0200

    Initial commit

---
+++ cmd/client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"net"
+	"strings"
+)
+
+func solvePoW(nonce string, difficulty int) string {
+	prefix := strings.Repeat("0", difficulty)
+	var counter int
+	for {
+		data := fmt.Sprintf("%s%d", nonce, counter)
+		hash := sha256.Sum256([]byte(data))
+		hashStr := hex.EncodeToString(hash[:])
+		if strings.HasPrefix(hashStr, prefix) {
+			return fmt.Sprintf("%d", counter)
+		}
+		counter++
+	}
+}
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+	n, _ := conn.Read(buffer)
+
+	var challenge Challange
+	json.Unmarshal(buffer[:n], &challenge)
+
+	result := solvePoW(challenge.Nonce, challenge.Difficulty)
+	conn.Write([]byte(result))
+
+	n, _ = conn.Read(buffer)
+	wisdom := string(buffer[:n])
+	fmt.Println(wisdom)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/voltento/wisdom-words
+
+go 1.20
